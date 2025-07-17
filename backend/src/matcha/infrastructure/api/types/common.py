2a03985820b494c735dd23e4901b7dc7891d6af1
byanis42 <byanis@student.42.fr>
2025-07-17 18:33:06 +0200

    deletion

--- backend/src/matcha/infrastructure/api/types/common.py
+++
@@ -1,8 +0,0 @@
-from typing import Any, Literal
-
-from pydantic import BaseModel
-
-class Response(BaseModel):
-    status: Literal["success", "error"] = "success"
-    data: Any | None = None
-    message: str | None = None
--- backend/src/matcha/infrastructure/framework/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DomainException(Exception):
-    status_code: int = 500
-    detail: str = "An error occurred"
-    name: str = "Domain Exception"
-
-
-class HandlerNotFound(DomainException):
-    name: str = "Handler Not Found"
